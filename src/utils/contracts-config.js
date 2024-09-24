2b709143afe02eb1279b0487c0ee215965547cad
Aditya <adityam874@gmail.com>
2024-09-24 20:11:48 +0530

    dapp created

---
+++ src/utils/contracts-config.js
@@ -0,0 +1,5 @@
+
+      export const contractAddress = "0xa4d05A6A581b7Da1eA6CDe8d5FcEe2525CDa67e1"
+      export const ownerAddress = "0x9cc68505B2fa69a936E032B12c654E5bA8b52bcE"
+      export const networkDeployedTo = "11155111"
+    
